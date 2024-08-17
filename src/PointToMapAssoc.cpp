3f9d4e1baad0705def45b7663e226e35d918cb61
brytsknguyen <thienminh.npn@gmail.com>
2024-08-17 22:55:05 +0800

    moved the planarity and normal calculation to main code to avoid arithmetic error

--- src/PointToMapAssoc.cpp
+++ src/PointToMapAssoc.cpp
@@ -23,8 +23,8 @@ void PointToMapAssoc::AssociatePointWithMap(PointXYZIT &fRaw, Vector3d &finB, Ve
     static auto commonPred = ufopred::HasSurfel()
                           && ufopred::DepthMin(surfel_min_depth)
                           && ufopred::DepthMax(surfel_query_depth - 1)
-                          && ufopred::NumSurfelPointsMin(surfel_min_point)
-                          && ufopred::SurfelPlanarityMin(surfel_min_plnrty);
+                          && ufopred::NumSurfelPointsMin(surfel_min_point);
+                        //   && ufopred::SurfelPlanarityMin(surfel_min_plnrty);
     auto pred = commonPred
              && ufopred::Intersects(ufoSphere(ufoPoint3(finW(0), finW(1), finW(2)), surfel_intsect_rad));
 
@@ -51,16 +51,67 @@ void PointToMapAssoc::AssociatePointWithMap(PointXYZIT &fRaw, Vector3d &finB, Ve
     {
         auto const& surfel = Map.getSurfel(node);
 
-        int depth        = node.depth();
-        int numPoint     = surfel.getNumPoints();
-        double planarity = surfel.getPlanarity();
-        Vector3d mean    = ufo::math::toEigen(surfel.getMean());
-        Vector3d norm    = ufo::math::toEigen(surfel.getNormal());
+        int depth = node.depth();
+        int numPoint = surfel.getNumPoints();
+
+        std::array<double, 6> cov = surfel.getSymmetricCovariance();
+        double planarity = 0;
+        Vector3d norm(0, 0, 0);
+
+        auto AccessCov = [](const std::array<double, 6> &cov, double &planarity, Vector3d &norm) -> void
+        {
+            double a = cov[0]; double b = cov[3]; double c = cov[5]; double d = cov[1]; double e = cov[4]; double f = cov[2];
+
+            if(isnan(a) || isnan(b) || isnan(c) || isnan(d) || isnan(e) || isnan(f))
+            {
+                printf("Node has NaN Cov");
+                planarity = 0;
+                norm = Vector3d(0, 0, 0);
+            }
+
+            if(isnan(a) || isnan(b) || isnan(c) || isnan(d) || isnan(e) || isnan(f))
+            {
+                printf("Node has inf Cov");
+                planarity = 0;
+                norm = Vector3d(0, 0, 0);
+            }
+
+            ROS_ASSERT_MSG(!isnan(a) && !isnan(b) && !isnan(c) && !isnan(d) && !isnan(e) && !isnan(f)
+                           && isfinite(a) && isfinite(b) && isfinite(c) && isfinite(d) && isfinite(e) && isfinite(f),
+                           "%f, %f, %f, %f, %f, %f\n", a, b, c, d, e, f);
+
+            double const x_1 = a * a + b * b + c * c - a * b - a * c - b * c + 3 * (d * d + f * f + e * e);
+
+            double const x_2 = -(2 * a - b - c) * (2 * b - a - c) * (2 * c - a - b)
+                               + 9 * ((2 * c - a - b) * (d * d) + (2 * b - a - c) * (f * f) + (2 * a - b - c) * (e * e))
+                               - 54 * (d * e * f);
+
+            double const phi = 0 < x_2 ? std::atan(std::sqrt(4 * x_1 * x_1 * x_1 - x_2 * x_2) / x_2)
+                                       : (0 > x_2 ? std::atan(std::sqrt(4 * x_1 * x_1 * x_1 - x_2 * x_2) / x_2) + M_PI
+                                                  : M_PI_2);
+            assert(!isnan(phi) && isfinite(phi));
+
+            // Find egein values
+            double l_1 = (a + b + c - 2 * std::sqrt(x_1) * std::cos(phi / 3)) / 3;
+            double l_2 = (a + b + c + 2 * std::sqrt(x_1) * std::cos((phi + M_PI) / 3)) / 3;
+            double l_3 = (a + b + c + 2 * std::sqrt(x_1) * std::cos((phi - M_PI) / 3)) / 3;
+
+            f = 0 == f ? std::numeric_limits<float>::epsilon() : f;
+            double const m_1 = (d * (c - l_1) - e * f) / (f * (b - l_1) - d * e);            
+            planarity = 2 * (l_2 - l_1) / (l_1 + l_2 + l_3);
+            norm = Vector3d((l_1 - c - e * m_1) / f, m_1, 1);
+            norm.normalize();
+        };
+
+        AccessCov(cov, planarity, norm);
+
+        // double planarity = surfel.getPlanarity();
+        // Vector3d norm = ufo::math::toEigen(surfel.getNormal());
+        Vector3d mean = ufo::math::toEigen(surfel.getMean());
 
         if(    isnan(mean(0)) || isnan(mean(1)) || isnan(mean(2))
             || isnan(norm(0)) || isnan(norm(1)) || isnan(norm(2))
-            || planarity < 0.0 || planarity > 1.0
-            // || norm.norm() != 1.0
+            || planarity < surfel_min_plnrty || planarity > 1.0
           )
         {       
             // printf(KYEL "Node with anomaly surf: %d. m: %f, %f, %f. n: %f, %f, %f. rho: %f\n" RESET,
